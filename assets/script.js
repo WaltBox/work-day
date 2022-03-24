4d93e12a1dd182dc8400f5c0b85dc9741b9c3302
Walt Boxwell <waltboxwell@gmail.com>
2022-03-24 09:37:32 -0500

    Need to add time and date to top of page

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+
+var savBtn = $(".saveBtn");
+
+// Functions
+
+// current day is displayed at the top of the calendar
+moment(Date);
+$("#currentDay").textContent(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
+// each time block is color coded to indicate whether it is in the past, present, or future
+
+        // console.log(this); //each time-block
+// console.log(this); // each time-block
+
+
+
+
+// when I click the savebtn for that time block
+
+// console.log(this); // save button
+
+
+
+// then the text for that event is saved in local storage
+
+
+
+// When I refresh the page 
+// The saved events stay
+
+
+
+//console.log(this)
+// console.log(currHour)
+
+
+
