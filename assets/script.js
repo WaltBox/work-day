13c2ff19143e42eacb2b50e640fe5334a9bf4a0a
Walt Boxwell <waltboxwell@gmail.com>
2022-04-06 13:37:43 -0500

    Finished project

--- assets/script.js
+++ assets/script.js
@@ -4,13 +4,66 @@ var savBtn = $(".saveBtn");
 // Functions
 
 // current day is displayed at the top of the calendar
+var date = new Date()
 moment(Date);
-$("#currentDay").textContent(moment().format('MMMM Do YYYY, h:mm:ss a'));
+$("#currentDay").text(moment(date).format('MMMM Do YYYY, h:mm:ss a'));
 
 
 // each time block is color coded to indicate whether it is in the past, present, or future
+function timeBlockColor() {
+   var hour = moment().hours();
 
-        // console.log(this); //each time-block
+   $(".time-block").each(function() {
+       var currHour = parseInt($(this).attr("id"));
+
+       // console.log(this); //each time-block
+
+       if (currHour > hour) {
+           $(this).addClass("future");
+       } else if (currHour === hour) {
+           $(this).addClass("present");
+       } else {
+           $(this).addClass("past");
+       }
+   })
+};
+timeBlockColor();
+
+function applyText() {
+    // Loop through all of our text element based on ID. Start at 9 and stop at 5pm = 17 military time
+    for (let i = 9; i < 18; i++) {
+        // Temp is equal to the current value stored in localStorage with key as the id == i
+        var temp = window.localStorage.getItem(i.toString());
+        // If there is something sotred in local storage at that key, display it on the screen, otherwise leave blank
+        if (temp != null) {
+            $('#' + i).text(temp);
+        }
+    }
+}
+/*
+localStorage = {
+    key: value
+    key == time
+    value == string inside   
+}
+localStorage = {
+    9: 'this is a test'
+    10: ''
+    11: ''
+    ...
+    17: ''
+}
+setItem(key, value)
+setItem(9, 'this is a test')
+*/
+
+applyText();
+
+function saveText(id) {
+    window.localStorage.setItem(id.toString(), $("#" + id.toString()).val());
+}
+
+   // console.log(this); //each time-block
 // console.log(this); // each time-block
 
 
